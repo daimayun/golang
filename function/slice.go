722dbfc947df13663e478e136b7a598f0522b603
wangben <wangben-1203@qq.com>
2022-06-25 23:37:29 +0800

    function

---
+++ function/slice.go
@@ -0,0 +1,39 @@
+package function
+
+// InSlice 判断元素是否在切片内[php:in_array]
+func InSlice[T comparable](slice []T, target T) bool {
+	for _, v := range slice {
+		if v == target {
+			return true
+		}
+	}
+	return false
+}
+
+// SliceUnique 切片去重[php:array_unique]
+func SliceUnique[T comparable](slice []T) []T {
+	if len(slice) <= 1 {
+		return slice
+	}
+	var (
+		newSlice = make([]T, 0)
+		m        = make(map[T]interface{})
+	)
+	for _, v := range slice {
+		if _, ok := m[v]; ok {
+			continue
+		}
+		m[v] = nil
+		newSlice = append(newSlice, v)
+	}
+	return newSlice
+}
+
+// SliceSum 切片元素求和[php:array_sum]
+func SliceSum[T uint | uint8 | uint16 | uint32 | uint64 | int | int8 | int16 | int32 | int64 | float32 | float64](slice []T) T {
+	var sum T = 0
+	for _, v := range slice {
+		sum += v
+	}
+	return sum
+}
