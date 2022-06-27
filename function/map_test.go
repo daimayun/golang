d14dc8ea6f888eb9f4304f89b3c701a3ded90789
wangben <1403375030@qq.com>
2022-06-27 18:24:03 +0800

    map

--- function/map_test.go
+++ function/map_test.go
@@ -1,1 +1,16 @@
 package function
+
+import "testing"
+
+func TestMapKeys(t *testing.T) {
+	testMap := struct {
+		m    map[string]string
+		keys []string
+	}{
+		map[string]string{"a": "one", "b": "two", "c": "three"},
+		[]string{"a", "b", "c"},
+	}
+	if actual := MapKeys(testMap.m); len(actual) != len(testMap.keys) {
+		t.Error("MapKeys() error.")
+	}
+}
