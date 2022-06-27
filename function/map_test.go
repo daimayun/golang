2bbcaf68a7962a3effa17e363e5135ff4c9a487f
wangben <wangben-1203@qq.com>
2022-06-27 22:10:21 +0800

    map

--- function/map_test.go
+++ function/map_test.go
@@ -13,4 +13,37 @@ func TestMapKeys(t *testing.T) {
 	if actual := MapKeys(testMap.m); len(actual) != len(testMap.keys) {
 		t.Error("MapKeys() error.")
 	}
+	testIntMap := struct {
+		m    map[string]int
+		keys []string
+	}{
+		map[string]int{"aa": 1, "bb": 2, "cc": 3},
+		[]string{"aa", "bb", "cc"},
+	}
+	if actual := MapKeys(testIntMap.m); len(actual) != len(testIntMap.keys) {
+		t.Error("MapKeys() error.")
+	}
+}
+
+func TestMapValues(t *testing.T) {
+	testMap := struct {
+		m    map[string]string
+		keys []string
+	}{
+		map[string]string{"a": "one", "b": "two", "c": "three"},
+		[]string{"one", "two", "three"},
+	}
+	if actual := MapValues(testMap.m); len(actual) != len(testMap.keys) {
+		t.Error("MapValues() error.")
+	}
+	testIntMap := struct {
+		m    map[string]int
+		keys []int
+	}{
+		map[string]int{"aa": 1, "bb": 2, "cc": 3},
+		[]int{1, 2, 3},
+	}
+	if actual := MapValues(testIntMap.m); len(actual) != len(testIntMap.keys) {
+		t.Error("MapValues() error.")
+	}
 }
