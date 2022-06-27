eb55d05f25b96ecd768b07e9b9e3e25b337b0f0b
wangben <wangben-1203@qq.com>
2022-06-27 22:17:54 +0800

    map

--- function/map_test.go
+++ function/map_test.go
@@ -27,23 +27,53 @@ func TestMapKeys(t *testing.T) {
 
 func TestMapValues(t *testing.T) {
 	testMap := struct {
-		m    map[string]string
-		keys []string
+		m      map[string]string
+		values []string
 	}{
 		map[string]string{"a": "one", "b": "two", "c": "three"},
 		[]string{"one", "two", "three"},
 	}
-	if actual := MapValues(testMap.m); len(actual) != len(testMap.keys) {
+	if actual := MapValues(testMap.m); len(actual) != len(testMap.values) {
 		t.Error("MapValues() error.")
 	}
 	testIntMap := struct {
-		m    map[string]int
-		keys []int
+		m      map[string]int
+		values []int
 	}{
 		map[string]int{"aa": 1, "bb": 2, "cc": 3},
 		[]int{1, 2, 3},
 	}
-	if actual := MapValues(testIntMap.m); len(actual) != len(testIntMap.keys) {
+	if actual := MapValues(testIntMap.m); len(actual) != len(testIntMap.values) {
 		t.Error("MapValues() error.")
+	}
+}
+
+func TestMapKeyExists(t *testing.T) {
+	testMap := struct {
+		m          map[string]string
+		key, value string
+		ok         bool
+	}{
+		map[string]string{"a": "one", "b": "two", "c": "three"},
+		"a",
+		"one",
+		true,
+	}
+	if actualValue, actualOk := MapKeyExists(testMap.m, testMap.key); actualValue != testMap.value || actualOk != testMap.ok {
+		t.Error("MapKeyExists() error.")
+	}
+	testIntMap := struct {
+		m     map[string]int
+		key   string
+		value int
+		ok    bool
+	}{
+		map[string]int{"aa": 1, "bb": 2, "cc": 3},
+		"cc",
+		3,
+		true,
+	}
+	if actualValue, actualOk := MapKeyExists(testIntMap.m, testIntMap.key); actualValue != testIntMap.value || actualOk != testIntMap.ok {
+		t.Error("MapKeyExists() error.")
 	}
 }
