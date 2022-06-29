b3cb62f63f02c5fc359a6c40a9ce534bbe48aed0
wangben <1403375030@qq.com>
2022-06-29 17:26:43 +0800

    file

--- function/file_test.go
+++ function/file_test.go
@@ -1,1 +1,21 @@
 package function
+
+import "testing"
+
+func TestFileExists(t *testing.T) {
+	file := "./file_test.go"
+	if actual := FileExists(file); actual == false {
+		t.Error("FileExists() error.")
+	}
+	file = "../conv"
+	if actual := FileExists(file); actual == false {
+		t.Error("FileExists() error.")
+	}
+}
+
+func TestIsFile(t *testing.T) {
+	file := "./file_test.go"
+	if actual := IsFile(file); actual == false {
+		t.Error("IsFile() error.")
+	}
+}
