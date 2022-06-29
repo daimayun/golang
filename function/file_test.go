ba6833533e0d8b62429de9669638c871fc0a98a3
wangben <1403375030@qq.com>
2022-06-29 17:28:40 +0800

    file

--- function/file_test.go
+++ function/file_test.go
@@ -18,4 +18,19 @@ func TestIsFile(t *testing.T) {
 	if actual := IsFile(file); actual == false {
 		t.Error("IsFile() error.")
 	}
+	file = "../conv"
+	if actual := IsFile(file); actual == true {
+		t.Error("IsFile() error.")
+	}
+}
+
+func TestIsDir(t *testing.T) {
+	file := "./file_test.go"
+	if actual := IsDir(file); actual == true {
+		t.Error("IsDir() error.")
+	}
+	file = "../conv"
+	if actual := IsDir(file); actual == false {
+		t.Error("IsDir() error.")
+	}
 }
