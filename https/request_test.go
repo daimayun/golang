f7172600a1003b4a5e45a7dc3b9ce13828b1ddaa
wangben <1403375030@qq.com>
2022-06-29 19:02:54 +0800

    https

--- https/request_test.go
+++ https/request_test.go
@@ -1,1 +1,16 @@
 package https
+
+import (
+	"net/http"
+	"net/url"
+	"testing"
+)
+
+func TestRequest(t *testing.T) {
+	host := "https://www.baidu.com"
+	var data url.Values = map[string][]string{"name": {"zs"}, "age": {"18"}, "sex": {"男"}}
+	str := data.Encode()
+	if _, actual := Request(http.MethodPost, host, &str, map[string]string{"Content-Type": "application/x-www-form-urlencoded"}); actual != nil {
+		t.Error("Request() error.")
+	}
+}
