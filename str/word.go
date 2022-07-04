48bbec307d605c68d7d855877a73f69b56b24efe
wangben <1403375030@qq.com>
2022-07-04 18:23:53 +0800

    str

---
+++ str/word.go
@@ -0,0 +1,74 @@
+package str
+
+import (
+	"strings"
+	"unicode"
+)
+
+// ToBigCamelCase 字符串转大驼峰格式
+func ToBigCamelCase(str string, signs ...string) (res string) {
+	sign := "_"
+	if len(signs) > 0 {
+		sign = signs[0]
+	}
+	strArr := strings.Split(str, sign)
+	for _, v := range strArr {
+		res += strings.ToUpper(string(v[0])) + v[1:]
+	}
+	return
+}
+
+// ToSmallCamelCase 字符串转小驼峰格式
+func ToSmallCamelCase(str string, signs ...string) (res string) {
+	sign := "_"
+	if len(signs) > 0 {
+		sign = signs[0]
+	}
+	strArr := strings.Split(str, sign)
+	for k, v := range strArr {
+		if k == 0 {
+			res += strings.ToLower(string(v[0])) + v[1:]
+		} else {
+			res += strings.ToUpper(string(v[0])) + v[1:]
+		}
+	}
+	return
+}
+
+// UcFirst 仅开头字母大写[将字符串中的第一个字母转换成大写]
+func UcFirst(str string) string {
+	for i, v := range str {
+		return string(unicode.ToUpper(v)) + str[i+1:]
+	}
+	return ""
+}
+
+// LcFirst 仅开头字母小写[将字符串中的第一个字母转换成小写]
+func LcFirst(str string) string {
+	for i, v := range str {
+		return string(unicode.ToLower(v)) + str[i+1:]
+	}
+	return ""
+}
+
+// UcWords 所有首字母大写[将字符串中每个单词的首字母转换成大写]
+func UcWords(str string) (res string) {
+	arr := strings.Split(str, " ")
+	sign := ""
+	for _, v := range arr {
+		res += sign + UcFirst(v)
+		sign = " "
+	}
+	return
+}
+
+// LcWords 所有首字母小写[将字符串中每个单词的首字母转换成小写]
+func LcWords(str string) (res string) {
+	arr := strings.Split(str, " ")
+	sign := ""
+	for _, v := range arr {
+		res += sign + LcFirst(v)
+		sign = " "
+	}
+	return
+}
