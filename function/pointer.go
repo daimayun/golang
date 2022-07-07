05ece04db6edd0bdbee062e5052773a390ed1f20
wangben <1403375030@qq.com>
2022-07-07 09:19:14 +0800

    pointer

--- function/pointer.go
+++ function/pointer.go
@@ -1,12 +1,12 @@
 package function
 
 // GetPointer 获取变量的指针地址
-func GetPointer[T int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | string | float32 | float64 | bool](v T) *T {
+func GetPointer[T any](v T) *T {
 	return &v
 }
 
 // GetPointerValue 根据指针地址获取变量的值
-func GetPointerValue[T int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | string | float32 | float64 | bool](vp *T) (v T) {
+func GetPointerValue[T any](vp *T) (v T) {
 	if vp == nil {
 		return
 	}
