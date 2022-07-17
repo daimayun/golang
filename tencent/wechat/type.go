cb25cff358ade25beb94fc6d0594b3baaa708d18
golangphp <wangben-1203@qq.com>
2022-07-17 10:57:06 +0800

    wechat

--- tencent/wechat/type.go
+++ tencent/wechat/type.go
@@ -11,3 +11,18 @@ type ResponseAccessTokenData struct {
 	AccessToken string `json:"access_token"`
 	ExpiresIn   int64  `json:"expires_in"`
 }
+
+// ResponseUserInfoData 获取用户基本信息(UnionID机制)
+type ResponseUserInfoData struct {
+	Subscribe      int     `json:"subscribe"`
+	OpenId         string  `json:"openid"`
+	Language       string  `json:"language"`
+	SubscribeTime  int64   `json:"subscribe_time"`
+	UnionId        string  `json:"unionid"`
+	Remark         string  `json:"remark"`
+	GroupId        int64   `json:"groupid"`
+	TagIdList      []int64 `json:"tagid_list"`
+	SubscribeScene string  `json:"subscribe_scene"`
+	QrScene        int64   `json:"qr_scene"`
+	QrSceneStr     string  `json:"qr_scene_str"`
+}
