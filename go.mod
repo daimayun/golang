ec7b71463d8a856b83bcdfdda75c57ab7f0f6657
wangben <1403375030@qq.com>
2022-08-03 14:02:57 +0800

    redis

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/daimayun/golang
 go 1.18
 
 require (
+	github.com/gomodule/redigo v1.8.9
 	gorm.io/driver/mysql v1.3.5
 	gorm.io/gorm v1.23.8
 )
---
+++ redis/connect.go
@@ -0,0 +1,91 @@
+package redis
+
+import (
+	"github.com/daimayun/golang/conv"
+	rds "github.com/gomodule/redigo/redis"
+	"time"
+)
+
+// redisServer Redis Server
+type redisServer struct {
+	Pool *rds.Pool
+}
+
+// Data 连接数据
+type Data struct {
+	Host           string        `json:"host"`
+	Port           int           `json:"port"`
+	Password       string        `json:"password"`
+	Database       int           `json:"database"`
+	NetWork        string        `json:"net_work"`
+	MaxIdle        int           `json:"max_idle"`
+	MaxActive      int           `json:"max_active"`
+	IdleTimeout    time.Duration `json:"idle_timeout"`
+	ReadTimeout    time.Duration `json:"read_timeout"`
+	WriteTimeout   time.Duration `json:"write_timeout"`
+	ConnectTimeout time.Duration `json:"connect_timeout"`
+}
+
+const (
+	DefaultHost           = "127.0.0.1"
+	DefaultPort           = 6379
+	DefaultMaxIdle        = 10
+	DefaultIdleTimeout    = time.Hour
+	DefaultNetWork        = "tcp"
+	DefaultReadTimeout    = time.Second
+	DefaultWriteTimeout   = time.Second
+	DefaultConnectTimeout = time.Second
+)
+
+// Rds Redis
+var Rds *redisServer
+
+func (data Data) Handel() Data {
+	if data.Host == "" {
+		data.Host = DefaultHost
+	}
+	if data.Port == 0 {
+		data.Port = DefaultPort
+	}
+	if data.MaxIdle == 0 {
+		data.MaxIdle = DefaultMaxIdle
+	}
+	if data.NetWork == "" {
+		data.NetWork = DefaultNetWork
+	}
+	if data.IdleTimeout == 0 {
+		data.IdleTimeout = DefaultIdleTimeout
+	}
+	if data.ReadTimeout == 0 {
+		data.ReadTimeout = DefaultReadTimeout
+	}
+	if data.WriteTimeout == 0 {
+		data.WriteTimeout = DefaultWriteTimeout
+	}
+	if data.ConnectTimeout == 0 {
+		data.ConnectTimeout = DefaultConnectTimeout
+	}
+	return data
+}
+
+// NewRedis 实例化Redis
+func NewRedis(data Data) {
+	data = data.Handel()
+	Rds = new(redisServer)
+	Rds.Pool = &rds.Pool{
+		MaxIdle:     data.MaxIdle,
+		MaxActive:   data.MaxActive,
+		IdleTimeout: data.IdleTimeout,
+		Dial: func() (rds.Conn, error) {
+			return rds.Dial(
+				data.NetWork,
+				data.Host+":"+conv.IntToString(data.Port),
+				rds.DialReadTimeout(data.ReadTimeout),
+				rds.DialWriteTimeout(data.WriteTimeout),
+				rds.DialConnectTimeout(data.ConnectTimeout),
+				rds.DialPassword(data.Password),
+				rds.DialDatabase(data.Database),
+			)
+		},
+	}
+}
