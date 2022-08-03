ec1b5c96ce4f882ae46167e6240b408db7ef49ad
wangben <1403375030@qq.com>
2022-08-03 14:45:38 +0800

    int

--- conv/int.go
+++ conv/int.go
@@ -27,3 +27,43 @@ func Int64ToString(i64 int64) string {
 func Int64ToFloat64(i64 int64) float64 {
 	return float64(i64)
 }
+
+// Int8ToInt64 int8转int64
+func Int8ToInt64(i8 int8) int64 {
+	return int64(i8)
+}
+
+// Int16ToInt64 int16转int64
+func Int16ToInt64(i16 int16) int64 {
+	return int64(i16)
+}
+
+// Int32ToInt64 int32转int64
+func Int32ToInt64(i32 int32) int64 {
+	return int64(i32)
+}
+
+// UintToInt uint转int
+func UintToInt(ui uint) int {
+	return int(ui)
+}
+
+// Uint8ToInt64 uint8转int64
+func Uint8ToInt64(ui8 uint8) int64 {
+	return int64(ui8)
+}
+
+// Uint16ToInt64 uint16转int64
+func Uint16ToInt64(ui16 uint16) int64 {
+	return int64(ui16)
+}
+
+// Uint32ToInt64 uint32转int64
+func Uint32ToInt64(ui32 uint32) int64 {
+	return int64(ui32)
+}
+
+// Uint64ToInt64 uint64转int64
+func Uint64ToInt64(ui64 uint64) int64 {
+	return int64(ui64)
+}
