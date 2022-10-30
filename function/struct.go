e3b1b4fce3a525b94c58dbe982df6645ee778ed8
wangben <wangben-1203@qq.com>
2022-10-30 13:54:31 +0800

    func:struct

--- function/struct.go
+++ function/struct.go
@@ -3,11 +3,11 @@ package function
 import "reflect"
 
 // GetStructTag 获取结构体某个字段的标签值
-func GetStructTag(s any, field, key string) (val string, ok bool) {
+func GetStructTag(s any, field, tag string) (val string, ok bool) {
 	var structField reflect.StructField
 	structField, ok = reflect.TypeOf(s).FieldByName(field)
 	if ok {
-		val, ok = structField.Tag.Lookup(key)
+		val, ok = structField.Tag.Lookup(tag)
 	}
 	return
 }
