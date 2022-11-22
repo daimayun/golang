54c9ea336a5d4f9d6c0b0ed0debd0f09f2637c01
wangben <wangben-1203@qq.com>
2022-11-22 11:45:20 +0800

    system:lock

---
+++ lock/system.go
@@ -0,0 +1,33 @@
+package lock
+
+import "time"
+
+// memoryCache 内存缓存
+var memoryCache map[interface{}]struct{} = make(map[interface{}]struct{})
+
+// GetSet 读取并加锁
+func GetSet(key interface{}, ttl ...uint64) (isExistLock bool) {
+	var delTtl uint64
+	if len(ttl) > 0 {
+		delTtl = ttl[0]
+	}
+	_, isExistLock = memoryCache[key]
+	if isExistLock {
+		return
+	}
+	memoryCache[key] = struct{}{}
+	if delTtl == 0 {
+		return
+	}
+	go func(key interface{}, ttl uint64) {
+		time.AfterFunc(time.Second*time.Duration(ttl), func() {
+			delete(memoryCache, key)
+		})
+	}(key, delTtl)
+	return
+}
+
+// Del 删除锁
+func Del(key interface{}) {
+	delete(memoryCache, key)
+}
