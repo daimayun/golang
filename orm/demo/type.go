55e62dc865800c705f90a1d7ee4af00fd4f74902
wangben <wangben-1203@qq.com>
2023-04-19 09:30:44 +0800

    orm:demo

--- orm/demo/type.go
+++ orm/demo/type.go
@@ -10,6 +10,7 @@ type User struct {
 	ID        int64      `json:"id" gorm:"column:id;primaryKey;not_null;autoIncrement;comment:ID"`
 	Name      string     `json:"name" gorm:"type:varchar(255);comment:用户名"`
 	Image     orm.List   `json:"image" gorm:"type:varchar(1000);comment:用户图片列表"`
+	Mix       orm.List   `json:"mix" gorm:"type:varchar(1000);comment:混合存储列表"`
 	CreatedAt orm.MyTime `json:"created_at" gorm:"type:datetime;comment:创建日期"`
 	UpdatedAt time.Time  `json:"updated_at" gorm:"type:datetime;comment:更新日期"`
 }
@@ -36,4 +37,13 @@ func test() {
 		map[string]string{"age": "18"},
 	}
 	u.Image = imageList
+
+	mix := orm.List{
+		"https://www.image.com/1.jpg",
+		1,
+		1.23,
+		[]string{"https://www.image.com/1.jpg"},
+		map[string]string{"name": "张三"},
+	}
+	u.Mix = mix
 }
