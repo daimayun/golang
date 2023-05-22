129e44bf7d015d57af671bcf00959f5485fe5090
wangben <wangben-1203@qq.com>
2023-05-22 16:36:24 +0800

    redis:lock

--- redis/lock.go
+++ redis/lock.go
@@ -7,5 +7,5 @@ import (
 
 // GetRedisLock 获取REDIS锁
 func GetRedisLock(key string, expireTime time.Duration) *redsync.Mutex {
-	return redisLock.NewMutex(key, redsync.SetExpiry(expireTime))
+	return redisLock.NewMutex(key, redsync.SetExpiry(expireTime), redsync.SetTries(1000))
 }
